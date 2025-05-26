362e9ee7eb1a82466500eb9800486a56b6d6511c
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-05-26 22:46:44 +0000

    Added code.py and completed the "Hello, World!" program.

---
+++ code.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: May 26, 2025
+# This main file is for the Space
+# Aliens program on the PyBadge.
+
+# Import the ugame module for
+# managing sounds and controls.
+# import ugame
+
+# Import the stage module for
+# screen display functions.
+# import stage
+
+
+# Define the main game scene function to run the whole game.
+def game_scene():
+    # Display three newlines for formatting.
+    print("\n\n\n")
+    # Display the customized greeting.
+    print("Hello, Joseph!")
+
+    # Construct an infinite loop.
+    while True:
+        # Do nothing by using pass as a placeholder for now.
+        pass
+
+
+# Check if the special name of the file is __main__.
+if __name__ == "__main__":
+    # Run the main game scene function if so.
+    game_scene()
