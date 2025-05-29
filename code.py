34adcf84daf104335846bb5ab68112c9cf54f204
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-05-29 15:55:14 +0000

    Modified comments and added other functions in code.py.

--- code.py
+++ code.py
@@ -6,33 +6,33 @@
 
 # Import the ugame module for
 # managing sounds and controls.
-# import ugame
+import ugame
 
 # Import the stage module for
 # screen display functions.
-# import stage
+import stage
 
 
 # Define the main game scene function to run the whole game.
 def game_scene():
     # Import a 16-bit bitmap image which contains
     # the background image bank for the game.
-    # image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
     # Create a grid of the image background with a
     # 10x8 tile grid on the PyBadge display, representing
     # its full size of 16x16 images it can contain.
-    # background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_background, 10, 8)
 
-    # Refresh the display at a 60 Hz frequency.
-    # game = stage.Stage(ugame.display, 60)
+    # Refresh the display at a 60 Hz frequency (60 FPS).
+    game = stage.Stage(ugame.display, 60)
 
-    # Add the background image to the list of ordered layers.
-    # game.layers = [background]
+    # Add the background grid to the list of ordered layers.
+    # Defaults to taking the first image.
+    game.layers = [background]
 
-    # Render all the sprites per game scene by
-    # showing all the sprites on the layers list.
-    # game.render_block()
+    # Render the layers onto the screen.
+    game.render_block()
 
     # Construct an infinite loop.
     while True:
