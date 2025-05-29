2978b5fcf55d39e0fcfef762920b7a5861fa1c19
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-05-29 02:10:13 +0000

    Commit for saving. Almost done images in code.py.

--- code.py
+++ code.py
@@ -15,10 +15,14 @@
 
 # Define the main game scene function to run the whole game.
 def game_scene():
-    # Display three newlines for formatting.
-    print("\n\n\n")
-    # Display the customized greeting.
-    print("Hello, Joseph!")
+    # Get an image bank from a 16-bit bitmap
+    # to use a background bank for the game.
+    # image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    # Set the background to image 0 of the background bank
+    # with a 10x8 grid on the PyBadge display, representing
+    # its full size of 16x16 images it can contain.
+    # background = stage.Grid(image_bank_background, 10, 8)
 
     # Construct an infinite loop.
     while True:
