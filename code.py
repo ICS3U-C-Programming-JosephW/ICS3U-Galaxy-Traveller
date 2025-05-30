a08f829be669dae610d3ae628e60618cc4069e9b
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-05-30 16:57:13 +0000

    Finished boundaries program in code.py.

--- code.py
+++ code.py
@@ -12,6 +12,10 @@ import ugame
 # screen display functions.
 import stage
 
+# Import the constants file
+# for useful constants.
+import constants
+
 
 # Define the main game scene function to run the whole game.
 def game_scene():
@@ -25,16 +29,20 @@ def game_scene():
     # Create a grid of the image background with a
     # 10x8 tile grid on the PyBadge display, representing
     # its full size of 16x16 images it can contain.
-    background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
 
-    # Create a ship sprite by selecting the fifth
-    # image off the sprite image bank. Set its
-    # position to 75 in the x and 66 in the y
-    # from the origin.
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    # Create a ship sprite by selecting the sixth
+    # image off the sprite image bank (0 index). Set
+    # its position to 75 in the x and double its size
+    # away from the bottom of the screen in the y.
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
 
     # Refresh the display at a 60 Hz frequency (60 FPS).
-    game = stage.Stage(ugame.display, 60)
+    game = stage.Stage(ugame.display, constants.FPS)
 
     # Set the layers with ordered items.
     game.layers = [ship] + [background]
@@ -51,53 +59,73 @@ def game_scene():
         # Check if the user pressed the
         # 'A' button on the PyBadge.
         if keys & ugame.K_X:
-            # Display the resulting
-            # button pressed, 'A.'
-            print("A")
+            # Do nothing by using pass
+            # as a placeholder for now.
+            pass
         # Check if the user pressed the
         # 'B' button on the PyBadge.
         if keys & ugame.K_O:
-            # Display the resulting
-            # button pressed, 'B.'
-            print("B")
+            # Do nothing by using pass
+            # as a placeholder for now.
+            pass
         # Check if the user pressed the
         # 'Start' button on the PyBadge.
         if keys & ugame.K_START:
-            # Display the resulting
-            # button pressed, 'Start.'
-            print("Start")
+            # Do nothing by using pass
+            # as a placeholder for now.
+            pass
         # Check if the user pressed the
         # 'Select' button on the PyBadge.
         if keys & ugame.K_SELECT:
-            # Display the resulting
-            # button pressed, 'Select.'
-            print("Select")
+            # Do nothing by using pass
+            # as a placeholder for now.
+            pass
         # Check if the user pressed the
         # keypad right button on the PyBadge.
         if keys & ugame.K_RIGHT:
-            # Move the ship one pixel
-            # right from the origin.
-            ship.move(ship.x + 1, ship.y)
+            # Check if the x-coordinate of the
+            # ship is within the length of the
+            # screen, meaning on the screen.
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                # Move the ship by its movement speed right from the origin.
+                ship.move(ship.x + constants.SPRITE_MOVEMENT_SPEED, ship.y)
+            # Otherwise, the ship is not on the
+            # screen and it is out of the screen
+            # length.
+            else:
+                # Reset the position of the ship
+                # to the right edge of the screen,
+                # and subtract it by the sprite size.
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
         # Check if the user pressed the
         # keypad left button on the PyBadge.
         if keys & ugame.K_LEFT:
-            # Move the ship one pixel
-            # left towards the origin.
-            ship.move(ship.x - 1, ship.y)
+            # Check if the ship is in the
+            # positive x range or zero,
+            # meaning on the screen.
+            if ship.x >= 0:
+                # Move the ship by its movement speed left from the origin.
+                ship.move(ship.x - constants.SPRITE_MOVEMENT_SPEED, ship.y)
+            # Otherwise, it is not on the screen
+            # and the x-coordinate of the ship is
+            # negative.
+            else:
+                # Reset the position of the ship
+                # to zero, keeping it on the left
+                # edge of the screen.
+                ship.move(0, ship.y)
         # Check if the user pressed the
         # keypad up button on the PyBadge.
         if keys & ugame.K_UP:
-            # Move the ship one pixel
-            # up towards the origin, where
-            # up is negative.
-            ship.move(ship.x, ship.y - 1)
+            # Do nothing by using pass
+            # as a placeholder for now.
+            pass
         # Check if the user pressed the
         # keypad down button on the PyBadge.
         if keys & ugame.K_DOWN:
-            # Move the ship one pixel
-            # down from the origin, where
-            # down is positive.
-            ship.move(ship.x, ship.y + 1)
+            # Do nothing by using pass
+            # as a placeholder for now.
+            pass
 
         # Redraw the sprites, with
         # only the ship for now.
