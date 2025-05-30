7a8458e2f69cace7d9b5c8d14f9927d49655e4d0
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-05-30 16:19:41 +0000

    Added constants.py file with constants.

---
+++ constants.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Created By: Joseph Wondimagnehu
+# Date: May 30, 2025
+# This module contains constants.
+
+# Set a constant for the screen length.
+SCREEN_X = 160
+# Set a constant for the screen width.
+SCREEN_Y = 128
+# Set a constant for the amount of grids lengthwise.
+SCREEN_GRID_X = 10
+# Set a constant for the amount of grids widthwise.
+SCREEN_GRID_Y = 8
+# Set a constant for the sprite size in pixels.
+SPRITE_SIZE = 16
+# Set a constant for the frames per second (frequency).
+FPS = 60
+# Set a constant for the movement speed of the sprite in pixels.
+SPRITE_MOVEMENT_SPEED = 1
